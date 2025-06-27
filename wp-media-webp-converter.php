42cf0c0cd1ca964ca68580b19e8272cc0ff9fbf1
emfluenceindia <ssarkar@emfluence.com>
2025-06-27 18:39:00 +0530

    Icons added

--- wp-media-webp-converter.php
+++ wp-media-webp-converter.php
@@ -5,6 +5,7 @@
  * Version: 1.0.0
  * Author: Subrata Sarkar
  * Author URI: https://github.com/emfluenceindia
+ * Plugin URI: https://github.com/emfluenceindia/WP-Media-WebP-Converter
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: wp-media-webp-converter
